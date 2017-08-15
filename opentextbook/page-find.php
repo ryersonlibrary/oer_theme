7c39abf580e0de5d6d999ef95c60188fc0eb1e8d
matt milner <milner.matthew@gmail.com>
2017-08-15 17:10:14 -0230

    Rebuilt Discovery UI js
    Rebuilt the Discovery UI actions / functions to quasi-DC specs. Not
    ready for live action, but closer.

--- opentextbook/page-find.php
+++ opentextbook/page-find.php
@@ -16,23 +16,12 @@ use Roots\Sage\Wrapper;
                     	<h5>Subject</h5>
                     	<ul id="discovery-ui-subject" data-param="subject">
                     		<li data-term="" class="subjectfacet">All Subjects</li>
-                    		<li data-term="art" class="subjectfacet">Art</li>
-                    		<li data-term="health" class="subjectfacet">Health</li>
-    	               		<li data-term="recreation" class="subjectfacet">Recreation</li>
-    	               		<li data-term="science" class="subjectfacet">Science</li>
-    	               		<li data-term="biology" class="subjectfacet">Biology</li>
-    	               		<li data-term="music" class="subjectfacet">Music</li>
-    	               		<li data-term="engineering" class="subjectfacet">Engineering</li>
-    	               		<li data-term="history" class="subjectfacet">History</li>
-    	               		<li data-term="information-studies" class="subjectfacet">Information Studies</li>
-    	               		<li data-term="classics" class="subjectfacet">Classics</li>
-    	               		<li data-term="medicine" class="subjectfacet">Medicine</li>
                     	</ul>
                     	<p class="showmore" id="discovery-moresubjects">Show 5 More Subjects</p>
                     	<h5>Type</h5>
                     	<ul id="discovery-ui-type" data-param="type">
                     		<li data-term="" class="typefacet">All Types</li>
-                    		<li data-term="reader" class="typefacet">Course Reader</li>
+                    		<li data-term="reader" class="typefacet">Course Readers</li>
                     		<li data-term="instruction" class="typefacet">Instructions</li>
     	               		<li data-term="lesson" class="typefacet">Lesson Plans</li>
                     	</ul>
@@ -50,7 +39,7 @@ use Roots\Sage\Wrapper;
     	               		<li data-term="fr" class="languagefacet">French</li>
                     	</ul>
                     	<h5>Date of Creation</h5>
-                    	<ul id="discovery-ui-created" data-param="created">
+                    	<ul id="discovery-ui-date" data-param="date">
                     		<li data-term="" class="createdfacet">Any Date</li>
                     		<li data-term="3months" class="createdfacet">Past 3 Months</li>
                     		<li data-term="year" class="createdfacet">Past Year</li>
@@ -64,14 +53,14 @@ use Roots\Sage\Wrapper;
     	               		<li data-term="unreviewed" class="peerreviewfacet">Unreviewed Items Only</li>
                     	</ul>
                     	<h5>Adoption Status</h5>
-                    	<ul id="discovery-ui-adoption" data-param="adoption">
+                    	<ul id="discovery-ui-source" data-param="source">
                     		<li data-term="" class="adoptionfacet">Adopted and Unadopted</li>
                     		<li data-term="mostadopted" class="adoptionfacet">Most Adopted</li>
                     		<li data-term="adopted" class="adoptionfacet">Adopted Items Only</li>
     	               		<li data-term="unadopted" class="adoptionfacet">Unadopted Items Only</li>
                     	</ul>
                     	<h5>Conditions of Use</h5>
-                    	<ul id="discovery-ui-use" data-param="use">
+                    	<ul id="discovery-ui-rights" data-param="rights">
                     		<li data-term="" class="usefacet">All Usage Conditions</li>
                     		<li data-term="nsa" class="usefacet">No Strings Attached</li>
                     		<li data-term="remixshare" class="usefacet">Remix and Share</li>
@@ -85,7 +74,7 @@ use Roots\Sage\Wrapper;
             	</div>
             	<div id="discovery-results" class="col-lg-9 col-md-9">
                 	<div id="results-header" class="col-lg-10 col-md-10 col-md-offset-1">
-                		<h3>Displaying all <span class="searchterm">Post-Secondary</span> items in <span class="searchterm">Science</span> with the keyword "<span class="searchterm">Biodiversity</span>"</h3>
+                		<h3>Displaying <span class="searchterm" id="search-limitations"></span> items <span class="searchterm" id="search-subject"></span> <span class="searchterm" id="search-keyword"></span> <span class="searchterm" id="search-created"></span></h3>
                 	</div>
                 	<div id="results-box" class="col-lg-12 col-md-12">
                     	<div id="results-prev" class="col-lg-1 col-md-1">
@@ -93,51 +82,7 @@ use Roots\Sage\Wrapper;
                     	</div>
                     	<div id="results" class="col-lg-10 col-md-10">
                         	<div id="results-list" class="results-row col-lg-12 col-md-12">
-                        		<div class="result-item col-lg-4 col-md-4">
-                            		<span class="textbook">
-                            			<span class="textbook-cover"></span>
-                            			<p class="textbook-header">Science</p>
-                            			<h4 class="textbook-title">This is a textbook</h4>
-                            			<p class="textbook-authors">Some Author, Author, Author</p>
-                            			<p class="textbook-footer">&gt;&nbsp;&nbsp;<a href="">About this book</a></p>
-                            		</span>
-                        		</div>
-                        		<div class="result-item col-lg-4 col-md-4">
-                            		<span class="textbook">
-                            			<span class="textbook-cover"></span>
-                            			<p class="textbook-header">Science</p>
-                            			<h4 class="textbook-title">This is a textbook</h4>
-                            			<p class="textbook-authors">Some Author, Author, Author</p>
-                            			<p class="textbook-footer">&gt;&nbsp;&nbsp;<a href="">About this book</a> <img title="Peer Reviewed Textbook" class="reviewed-textbook" src="<?= get_template_directory_uri(); ?>/dist/images/ui-star-white.svg"></p>
-                            		</span>
-                        		</div>
-                        		<div class="result-item col-lg-4 col-md-4">
-                            		<span class="textbook">
-                            			<span class="textbook-cover"></span>
-                            			<p class="textbook-header">Science</p>
-                            			<h4 class="textbook-title">This is a textbook</h4>
-                            			<p class="textbook-authors">Some Author, Author, Author</p>
-                            			<p class="textbook-footer">&gt;&nbsp;&nbsp;<a href="">About this book</a></p>
-                            		</span>
-                            	</div>
-                        		<div class="result-item col-lg-4 col-md-4">
-                            		<span class="textbook">
-                            			<span class="textbook-cover"></span>
-                            			<p class="textbook-header">Science</p>
-                            			<h4 class="textbook-title">This is a textbook</h4>
-                            			<p class="textbook-authors">Some Author, Author, Author</p>
-                            			<p class="textbook-footer">&gt;&nbsp;&nbsp;<a href="">About this book</a> <img  title="Peer Reviewed Textbook" class="reviewed-textbook" src="<?= get_template_directory_uri(); ?>/dist/images/ui-star-white.svg"></p>
-                            		</span>
-                            	</div>
-                        		<div class="result-item col-lg-4 col-md-4">
-                            		<span class="textbook">
-                            			<span class="textbook-cover"></span>
-                            			<p class="textbook-header">Science</p>
-                            			<h4 class="textbook-title">This is a textbook</h4>
-                            			<p class="textbook-authors">Some Author, Author, Author</p>
-                            			<p class="textbook-footer">&gt;&nbsp;&nbsp;<a href="">About this book</a></p>
-                            		</span>
-                        		</div>
+
                     		</div>
                     	</div>
                     	<div id="results-next" class="col-lg-1 col-md-1">
@@ -146,10 +91,10 @@ use Roots\Sage\Wrapper;
                 	</div>
                 	<div id="results-footer" class="col-lg-12 col-md-12">
                 		<div id="results-pagecounter" class="col-lg-12 col-md-12">
-                			<p><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a><a class="resultspage" href="">1</a></p>
+                			<p id="results-pagecounter-pages"><span class="resultspage" id="resultspage-1">1</span><span class="resultspage" id="resultspage-2">2</span></p>
                 		</div>
                 		<div id="results-counterdetails" class="col-lg-12 col-md-12">
-                			<p>Showing 1 of 5 of 5 Results</p>
+                			<p>Showing <span id="results-counter-thispage">1</span> of <span id="results-counter-totalpages">5</span> of <span id="results-counter-totalresults">20</span> Results</p>
                 		</div>
                 	</div>
             	</div>
