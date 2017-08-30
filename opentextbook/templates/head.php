5a454aff6b1bd976c2fc766129d2952651afcc26
matt milner <milner.matthew@gmail.com>
2017-08-30 11:41:54 -0230

    EOT and Light Font fix
    - created EOT for Light versions
    - altered main font to Frutiger Light, added letter spacing for entire site

--- opentextbook/templates/head.php
+++ opentextbook/templates/head.php
@@ -3,4 +3,19 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//opentextbook-piwik.agile.dev.work/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
 </head>
