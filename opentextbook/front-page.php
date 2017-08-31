0040175ea9a8d18b53ee4137b2efce51cde54808
matt milner <milner.matthew@gmail.com>
2017-08-31 09:57:30 -0230

    CSS fixes
    - liquid heights for education boxes under 1076px
    - hover states for btns
    - cleared up btn CSS SCSS, and removed btn classes from btns, as now extended in SCSS files

--- opentextbook/front-page.php
+++ opentextbook/front-page.php
@@ -18,7 +18,7 @@ use Roots\Sage\Wrapper;
             			<hr>
                			<h3>Adopt</h3>
                			<p>Search our collection of open textbooks. This high-quality, curated collection features resources on some of the top subject areas in post-secondary education in Ontario today. Many of the textbooks have been reviewed and vetted by educators across Canada.</p>
-               			<p class="footer"><a href="" class="btn btn-inverted" id="adopt-btn">Find and Adopt</a></p>
+               			<p class="footer"><a href="" id="adopt-btn">Find and Adopt</a></p>
                		</div>
                	</div>
             	<div class="col-lg-4 col-md-4 col-md-offset-0 col-sm-6 col-sm-offset-3 col-xs-12">
@@ -26,7 +26,7 @@ use Roots\Sage\Wrapper;
                			<hr>
                			<h3>Adapt</h3>
                			<p>Reuse, remix and repurpose open textbooks to suit your unique teaching needs, take advantage of the open textbook Creative Commons license and customize an open textbook to help your students achieve their learning goals.</p>
-               			<p class="footer"><a href="" class="btn btn-secondary" id="adapt-btn">Find and Adapt</a></p>
+               			<p class="footer"><a href="" id="adapt-btn">Find and Adapt</a></p>
                    	</div>
                	</div>
             	<div class="col-lg-4 col-md-4 col-md-offset-0 col-sm-6 col-sm-offset-3 col-xs-12">
@@ -34,7 +34,7 @@ use Roots\Sage\Wrapper;
                			<hr>
                			<h3>Create</h3>
                			<p>Find all the tools you need to create your very own open learning resources including free publishing tools and support, as well as a way to connect with the growing community of open educational creators and editors.</p>
-               			<p class="footer"><a href="" class="btn btn-secondary" id="create-btn">Start Authoring</a></p>
+               			<p class="footer"><a href="" id="create-btn">Start Authoring</a></p>
                		</div>
                	</div>
             </div>
@@ -52,7 +52,7 @@ use Roots\Sage\Wrapper;
                     	<p>Become part of the global open educational resources (OER) community. The OER movement is playing a central role in making education affordable, putting higher education within reach for more Ontarians. Any individual who has attended college or university knows the financial burden of buying expensive textbooks. Help students and instructors lower the costs of higher education by becoming part of the community today.</p>
                 	</div>
                 	<div id="findoutmore-box" class="row">
-                    	<a href="" class="btn btn-primary" id="findoutmore-btn">Find out more</a>
+                    	<a href="" id="findoutmore-btn">Find out more</a>
                     </div>
 				</div>
 			</div>
@@ -82,7 +82,7 @@ use Roots\Sage\Wrapper;
             			<img src="<?= get_template_directory_uri(); ?>/dist/images/ecampuslogo-white-trans.png">
             		</div>
             		<div id="submitapplication-box" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            			<a href="" class="btn btn-inverted" id="peerreview-btn">Submit an Application</a>
+            			<a href="" id="peerreview-btn">Submit an Application</a>
             		</div>
             	</div>
             </div>
@@ -102,14 +102,14 @@ use Roots\Sage\Wrapper;
             			<div id="twitterfeed" class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
             				<h3><img src="<?= get_template_directory_uri(); ?>/dist/images/twitter-icon-green.png" id="openeducation-twitter-logo" alt="" title="Twitter Logo"></h3>
             				<p><a class="twitter-timeline" href="https://twitter.com/eCampusOntario">Tweets by eCampusOntario</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></p>
-            				<a href="https://twitter.com/eCampusOntario" class="btn btn-tertiary" id="twitter-btn">Visit our Twitter Feed</a>
+            				<a href="https://twitter.com/eCampusOntario" id="twitter-btn">Visit our Twitter Feed</a>
         	    		</div>
             			<div id="rssfeed" class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
             				<h3><img src="<?= get_template_directory_uri(); ?>/dist/images/rss-icon-green.png" id="openeducation-rss-logo" alt="" title="RSS Logo"></h3>
             				<?php
 echo do_shortcode( '[wp_rss_aggregator limit="10" links_before=\'<ul class="rss-aggregator">\' link_before=\'<li class="feed-item-link">\']' );
 ?>
-            				<a href="https://www.diigo.com/rss/profile/Clintlalonde/open?query=%23open&sort=updated" class="btn btn-tertiary" id="rss-btn">Subscribe to our RSS Feed</a>
+            				<a href="https://www.diigo.com/rss/profile/Clintlalonde/open?query=%23open&sort=updated" id="rss-btn">Subscribe to our RSS Feed</a>
             			</div>
             		</div>
                 </div>
