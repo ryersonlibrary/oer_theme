392f0894e7947e5ebd29b80b90bb96a95685608e
Bill Kennedy <bill@intelligentmachines.ca>
2017-09-03 18:23:08 -0400

    Javascript Restructuring
    - Changed file.js to discovery.js
    - Refactored discovery.js to be a standalone file (needed to remove the jQuery function wrapper
    - Created catalogue.js to instantiate the discovery class
    - This allows the discovery class to also be instantiated on the preview page.
    
    Signed-off-by: Bill Kennedy <bill@intelligentmachines.ca>

--- opentextbook/assets/scripts/catalogue.js
+++ opentextbook/assets/scripts/catalogue.js
@@ -2,9 +2,9 @@
 //needed for function hoisting for build 
 
 (function($) {
-      
+        
     $(document).ready(function() {
-            
+                  
       var discovery = new ECommonsOntarioDiscovery
         ({
           dbURI:        'dsweb.semiprodint.ryerson.ca/rest',
--- opentextbook/dist/scripts/catalogue.js
+++ opentextbook/dist/scripts/catalogue.js
@@ -1,2 +1,2 @@
 'use strict';(function($){$(document).ready(function(){var discovery=new ECommonsOntarioDiscovery({dbURI:'dsweb.semiprodint.ryerson.ca/rest',dbmethod:'http'});discovery.init()})})(jQuery);
-//# sourceMappingURL=preview.js.map
+//# sourceMappingURL=catalogue.js.map
